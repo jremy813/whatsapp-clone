8dba5680ec8d4002539e5cea45d24f3d18a31213
jremy813 <jremywatkins@gmail.com>
2020-08-24 19:03:41 -0400

    added google sign in authentication with firebase

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./Login";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     //BEM naming convention
     <div className="App">
-      <h1>WhatsApp Clone!</h1>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
 
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Login.css";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "./firebase";
+
+function Login() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => console.log(result))
+      .catch((error) => alert(error.message));
+  };
+  return (
+    <div>
+      <div className="login__container">
+        <img
+          src="https://sharpcctv.co.uk/wp-content/uploads/2020/05/WhatsApp_logo_logotype_text.png"
+          alt=""
+        />
+        <div className="login__text">
+          <h1>Sign in to WhatsApp</h1>
+        </div>
+
+        <Button onClick={signIn}>Sign In With Google</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
