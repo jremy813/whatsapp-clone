a024d89e28962f17b5d54434e6fb676341a7d92f
jremy813 <jremywatkins@gmail.com>
2020-08-24 14:22:02 -0400

    structured app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    //BEM naming convention
+    <div className="App">
+      <h1>WhatsApp Clone!</h1>
+
+      <div className="app__body">
+        {/* Sidebar */}
+        {/* Chat */}
+      </div>
+    </div>
+  );
 }
 
 export default App;
