904ce077127a4b396b6d82579ef93b206663c968
jremy813 <jremywatkins@gmail.com>
2020-08-24 13:51:33 -0400

    started create-react-app and added firebase config file

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9Uxi4P8YLqu7oyAYNoDydMz3oyR7A-ds",
+  authDomain: "whats-app-clone-c5a5b.firebaseapp.com",
+  databaseURL: "https://whats-app-clone-c5a5b.firebaseio.com",
+  projectId: "whats-app-clone-c5a5b",
+  storageBucket: "whats-app-clone-c5a5b.appspot.com",
+  messagingSenderId: "544627770468",
+  appId: "1:544627770468:web:4a4da58a1e369aae7fcb71",
+  measurementId: "G-6CYF1ME0DR",
+};
